5b355c30e86b2e6c29d3555a360cdb6e5c983610
Acker <wanggaigezju@gmail.com>
2017-07-06 23:51:31 +0800

    update help map

--- search.js
+++ search.js
@@ -10,3 +10,25 @@ var array = [
 	[-1,  8, 10, 11, 12, 12, -1],
 	[-1, -1, -1, -1, -1, -1, -1],
 ]
+
+var len = array.length;
+var big_corner = 100;
+var big_distance = 10000;
+function create_help_array(map){
+	var len = map.length;
+	var arr = new Array(len);
+	map.forEach((d, i) => {
+		arr[i] = [];
+		d.forEach((e, j) => {
+			arr[i][j] = {value: e, minCrossing: big_corner, minDistance: big_distance, lastPos: undefined}
+		});
+	});
+	return arr;
+}
+// x: 1-> len - 2 ,  y: 1 -> len - 2  have picture.
+function search(begin, end, map){
+	if(begin == null || end == null) return false;
+	if(begin.x === end.x && begin.y === end.y) return true;
+	let help_map = create_help_array(map);
+	
+}
