9b5574ee3b346d8cd9438c2302e5cc879ce56901
Acker <wanggaigezju@gmail.com>
2017-07-09 23:45:20 +0800

    update img & try algorithm to layout

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
-//
-
 (function(map_width_height){
-	var pic_wh = 30;
+	if(map_width_height % 2 != 0) {
+		alert('Even rows required!');
+		return;
+	}
+	var pic_wh = 40;
 	var padding = 5;
 	var pre_src = './img/';
 	var pic_len = map_width_height * map_width_height / 2 > 30 ? 30 : parseInt(map_width_height * map_width_height / 2);
@@ -16,8 +17,12 @@
 	function cal_data(){
 		var pic_array = [], id, sub_x, sub_y;
 		var pic_array_len = map_width_height * map_width_height;
+		var all_row_columns = [];
+		for (var i = 1; i <= map_width_height; i++) 
+			for(var j = 1; j <= map_width_height; j++)
+				all_row_columns.push({x:i, y:j});
+		var has_selected = [], not_selected = all_row_columns;
 		for (var i = 0; i < pic_array_len; i++) {
-
 			id = i % (pic_len) + 1;
 			//start for(1, 1);
 			//11->(1, 1)->(2, 2)
@@ -30,10 +35,68 @@
 				src: pre_src + id + '.jpg', 
 				pos: {x: sub_x, y: sub_y},
 				space: find(sub_x, sub_y)
-				});
+			});
 		};
 		return pic_array;
 	}
+	function fetch_first_undistribute(sub_map){
+		for (var i = 0; i < sub_map.length; i++) {
+			if(!sub_map[i].distribute) return i;
+		};
+		return -1;
+	}
+	function init_map_layout(sub_map, distributed_num, pic_index){
+		if(sub_map.length == distributed_num) return true;
+		var id = (pic_index % pic_len) + 1;
+		var last = sub_map[fetch_first_undistribute(sub_map)];
+		last.distribute = true;
+		MAP[last.x][last.y] = id;
+		// if(distributed_num + 2 == sub_map.length){
+		// 	var index_one = fetch_first_undistribute(sub_map);
+		// 	sub_map[index_one].distribute = true;
+		// 	var index_two = fetch_first_undistribute(sub_map);
+		// 	sub_map[index_two].distribute = true;
+		// 	MAP[sub_map[index_one].x][sub_map[index_one].y] = pic_index;
+		// 	MAP[sub_map[index_two].x][sub_map[index_two].y] = pic_index;
+		// 	return true;
+		// }
+		for(var i = 0; i < sub_map.length; ++i){
+			var tmp = sub_map[i];
+			if(tmp.distribute) continue;
+			MAP[tmp.x][tmp.y] = id;
+			let re = search(MAP, last, tmp);
+			if(re.length <= 0){
+				last.distribute = false;
+				//MAP[last.x][last.y] = -1;
+				MAP[tmp.x][tmp.y] = -1;
+			}else{
+				last.distribute = tmp.distribute = true;
+				if(init_map_layout(sub_map, distributed_num + 2, pic_index + 1)){
+					return true;
+				}else{
+					MAP[tmp.x][tmp.y] = -1;
+					last.distributed = tmp.distribute = false;
+				}
+			}
+		}
+		MAP[last.x][last.y] = -1;
+		return false;
+	}
+	function generate_map(){
+		var sub_map = [];
+		for (var i = 1; i <= map_width_height; i++) {
+			for (var j = 1; j <= map_width_height; j++) {
+				sub_map.push({x: i, y: j, distribute: false});
+			};
+		};
+		sub_map = _.shuffle(sub_map);
+		debugger;
+		var re = init_map_layout(sub_map, 0, 1);
+		console.log(MAP);
+		
+	}
+	//generate_map();
+
 	/**
 	 * Calculate pixel width and height of the game window.
 	 *
@@ -80,18 +143,11 @@
 	var vm = new Vue({
 	    el: '#linkgame',
 	    data: {
-	      // pics: [
-	      //   {x: 0, y: 0, width: 30, height: 30, url: './img/1.jpg'},
-	      //   {x: 35, y: 0, width: 30, height: 30, url: './img/5.jpg'},
-	      //   {x: 70, y: 0, width: 30, height: 30, url: './img/7.jpg'},
-	      //   {x: 105, y: 0, width: 30, height: 30, url: './img/9.jpg'},
-	      // ],
 	      pics: cal_data(),
 	      space: cal_squ(map_width_height),
 	      path: '',
 	      select_one: undefined,
 	      select_two: undefined
-
 	    },
 	    methods: {
 	      hello: function(event){
@@ -103,7 +159,7 @@
 	        	this.select_two = target;
 	        	let which_one = get_which(this.select_one);
 	        	let which_two = get_which(this.select_two);
-	        	if(this.pics[which_one].id != this.pics[which_two].id){
+	        	if(this.pics[which_one].id != this.pics[which_two].id || which_one == which_two){
 	        		this.init_select();
 	        		return;
 	        	}else{
@@ -129,7 +185,7 @@
 	        				if(this.check()){
 	        					alert('Congratulations!');
 	        				}
-	        			}).bind(this), 500)
+	        			}).bind(this), 300)
 	        		}
 	        	}
 	        }
@@ -150,4 +206,4 @@
 	      }
 	    }
 	 });
-})(25);
+})(10);
